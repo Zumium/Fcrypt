1f1b53eefda6c10a40f09de2ed3b3829681c4629
Zumium <martin007323@gmail.com>
2015-03-01 18:29:47 +0800

    add license notices and text

--- ciphers.py
+++ ciphers.py
@@ -1,3 +1,24 @@
+##
+#    ciphers.py - offers the features about encryption/decryption
+#
+#    Copyright (C) 2015, Zumium <martin007323@gmail.com>
+#
+#This file is part of Fcrypt.
+#
+#    Fcrypt is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Fcrypt is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Fcrypt.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 from Crypto.Cipher import AES,Blowfish
 from Crypto import Random
 from Crypto.Hash import SHA256
